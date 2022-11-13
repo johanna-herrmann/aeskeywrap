7e8513a2d45b3a8c349420d82e72e0122f0dd71d
Mark Herrmann <mark.herrmann@mailbox.org>
2022-11-13 16:37:11 +0100

    remove test dummy

--- test/unit/indexTests.ts
+++
@@ -1,8 +0,0 @@
-import { assert } from 'assertthat';
-
-// Just a dummy suite with dummy test and dummy assert
-suite('dummy-test', (): void => {
-  test('does nothing.', async (): Promise<void> => {
-    assert.that(7).is.equalTo(7);
-  });
-});
